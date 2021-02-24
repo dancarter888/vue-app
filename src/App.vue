54c8b4481f86c8a14834bcbe8a7dc4172b74ef81
dancarter888 <49675893+dancarter888@users.noreply.github.com>
2021-02-24 16:46:05 +1300

    filterhead responisve

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,22 @@
   <div class="bg-green-light">
     <Navbar />
     <Main />
+    <FilterHeader />
   </div>
 </template>
 
 <script>
 import Navbar from './components/Navbar.vue'
 import Main from './components/Main.vue'
+import FilterHeader from './components/FilterHeader.vue'
 
 export default {
   name: 'App',
   components: {
     Navbar,
-    Main
+    Main,
+    FilterHeader,
+
   }
 }
 </script>
---
+++ src/components/FilterHeader.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="w-full h-50px md:h-60px lg:h-120px grid grid-cols-12 md:grid-cols-20 lg:grid-cols-36 gap-0 bg-green-forest items-center">
+      <div class="col-start-5 col-span-4 md:col-start-9 md:col-span-4 lg:col-start-17 flex justify-center">
+          <span class="text-white text-15px md:text-18px lg:text-20px">FILTER EVENTS</span>
+      </div>
+      <div class="col-start-9 col-span-3 md:col-start-18 md:col-span-2 lg:col-start-33 lg:col-span-3 flex justify-end">
+          <button class="text-green-light text-15px lg:text-16px">Show Filter</button>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'FilterHeader'
+
+}
+</script>
